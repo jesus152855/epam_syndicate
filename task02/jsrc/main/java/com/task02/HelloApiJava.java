d8ac1fc96b692d73fdd46201d6643a111aa45b77
Jesus Rodriguez <jrodriguez3@atlassian.com>
2024-07-08 16:18:16 -0500

    lambda function

---
+++ task02/jsrc/main/java/com/task02/HelloApiJava.java
@@ -0,0 +1,89 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+@LambdaHandler(lambdaName = "hello_api_java",
+	roleName = "hello_api_java-role",
+	isPublishVersion = true,
+    runtime = DeploymentRuntime.JAVA17,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
+)
+public class HelloApiJava implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
+	private static final int SC_OK = 200;
+    private static final int SC_NOT_FOUND = 404;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
+    private final Map<RouteKey, Function<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse>> routeHandlers = Map.of(
+            new RouteKey("GET", "/"), this::handleGetRoot,
+            new RouteKey("GET", "/hello"), this::handleGetHello
+    );
+
+    @Override
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
+        RouteKey routeKey = new RouteKey(getMethod(requestEvent), getPath(requestEvent));
+        return routeHandlers.getOrDefault(routeKey, this::notFoundResponse).apply(requestEvent);
+    }
+
+    private APIGatewayV2HTTPResponse handleGetRoot(APIGatewayV2HTTPEvent requestEvent) {
+        return buildResponse(SC_OK, ("Use the path /hello to get greetings message"));
+    }
+
+    private APIGatewayV2HTTPResponse handleGetHello(APIGatewayV2HTTPEvent requestEvent) {
+        return buildResponse(SC_OK, "Hello from Lambda");
+    }
+
+    private APIGatewayV2HTTPResponse notFoundResponse(APIGatewayV2HTTPEvent requestEvent) {
+        return buildResponse(SC_NOT_FOUND, "The resource with method %s and path %s is not found".formatted(
+                getMethod(requestEvent),
+                getPath(requestEvent)
+        ));
+    }
+
+    private APIGatewayV2HTTPResponse buildResponse(int statusCode, String body) {
+        return APIGatewayV2HTTPResponse.builder()
+                .withStatusCode(statusCode)
+                .withHeaders(responseHeaders)
+                .withBody(gson.toJson(body))
+                .build();
+    }
+
+    private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
+        return requestEvent.getRequestContext().getHttp().getMethod();
+    }
+
+    private String getPath(APIGatewayV2HTTPEvent requestEvent) {
+        return requestEvent.getRequestContext().getHttp().getPath();
+    }
+
+    private String getUserName(Map<String, String> queryStringParameters) {
+        return queryStringParameters.get("name");
+    }
+
+
+    private record RouteKey(String method, String path) {
+    }
+}
