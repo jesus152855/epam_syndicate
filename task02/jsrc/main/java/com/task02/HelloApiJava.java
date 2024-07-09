9ac74c8b5b7d977ed5918dba1659c1ffd37c8d30
Jesus Rodriguez <jrodriguez3@atlassian.com>
2024-07-08 19:26:03 -0500

    Fix lambda handler

--- task02/jsrc/main/java/com/task02/HelloApiJava.java
+++ task02/jsrc/main/java/com/task02/HelloApiJava.java
@@ -56,7 +56,7 @@ public class HelloApiJava implements RequestHandler<APIGatewayV2HTTPEvent, APIGa
     }
 
     private APIGatewayV2HTTPResponse notFoundResponse(APIGatewayV2HTTPEvent requestEvent) {
-        return buildResponse(SC_NOT_FOUND, new Body(400, "Bad request syntax or unsupported method. Request path: {%s}. HTTP method: {%s}".formatted(
+        return buildResponse(SC_NOT_FOUND, new Body(400, "Bad request syntax or unsupported method. Request path: %s. HTTP method: %s".formatted(
                 getPath(requestEvent),
                 getMethod(requestEvent)
         )));
